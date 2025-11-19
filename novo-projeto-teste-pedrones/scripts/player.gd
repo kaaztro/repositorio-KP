cd7599fe2dbd5755ad0489e49c80e4c83f8f6ebb
WhiteRose-ph <peagaduarte8@outlook.com>
2025-11-19 00:11:49 -0300

    Animações de pulo e movimento
    Adicionadas movimentações de pulo e movimento. Script player.gs chama  as animações ao movimentar.
    Animação prioritária corrige de volta para a animação IDLE quando o player parar de se mover ou não estiver mais pulando.

--- novo-projeto-teste-pedrones/scripts/player.gd
+++ novo-projeto-teste-pedrones/scripts/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_FORCE = -400.0
+const SPEED = 200.0
+const JUMP_FORCE = -350.0
 
+var is_jumping := false
+
+@onready var movementAnim := $AnimatedSprite2D as AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -15,13 +18,26 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_FORCE
 		#verifica se o botão de pulo foi pressionado e se o player esta no chão, e aplica gravidade negativa (pulo)
+		is_jumping = true
+	elif is_on_floor():
+		is_jumping = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	# Movement
+	var direction = Input.get_axis("ui_left", "ui_right")
+
+	velocity.x = direction * SPEED
+
+	if direction != 0:
+		movementAnim.scale.x = direction
+
+	# ANIMAÇÃO PRIORITÁRIA
+	if is_jumping:
+		movementAnim.play("jump")
+	elif direction != 0:
+		movementAnim.play("run")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		movementAnim.play("idle")
 
 	move_and_slide()
